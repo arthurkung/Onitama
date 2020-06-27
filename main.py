7b2cfb2e258b641fc6ca2625b71be58940cd99e0
Arthur Kung <arthurkung1006@gmail.com>
2020-06-27 17:57:06 +0100

    create board object

--- main.py
+++ main.py
@@ -32,6 +32,44 @@ class Player:
         for c in self.card_list:
             print(c)
 
+class Board:
+    def __init__(self, size,l_player,r_player):
+        self.piece_dict = {}
+        self.board_range = [(x,y) for x in range(size) for y in range(size)]
+        middle_loc = round(size/2)
+        l_x = 0
+        r_x = size - 1
+        for y in range(size):
+            if y == middle_loc:
+                self.piece_dict[(r_x,y)]=Master(r_player)
+                self.piece_dict[(l_x,y)]=Master(l_player)
+            else:
+                self.piece_dict[(l_x,y)]=Pawn(l_player)
+                self.piece_dict[(r_x,y)]=Pawn(r_player)
+
+    def get_piece(self,loc):
+        return self.piece_dict.get(loc,None)
+
+    def display(self):
+        output = 'Board:'
+        for loc in self.board_range:
+            *_ ,loc_y = loc
+            if loc_y == 0:
+                output = output + '\n'
+            unit = self.get_piece(loc)
+            if unit is None:
+                output = output + (' ~~ ')
+            else:
+                output = output + unit.__str__()
+
+        print(output)
+
+    def check_out_of_range(self,loc):
+        if loc in self.board_range:
+            return False
+        else:
+            return True
+
 class Game:
 
     def __init__(self):
@@ -39,32 +77,12 @@ class Game:
         self.l_player = Player('L',1)
         self.r_player = Player('R',-1)
         # set up board
-        self.board_range = ((x,y) for x in range(5) for y in range(5))
-        left_row = [Pawn(self.l_player) if i !=2 else Master(self.l_player) for i in range(5)]
-        right_row = [Pawn(self.r_player) if i !=2 else Master(self.r_player) for i in range(5)]
-        self.board = [left_row, [], [], [], right_row]
-        for i in range(1,4):
-            self.board[i] = [None for j in range(5)]
+        self.board = Board(5,self.l_player,self.r_player)
         #set up card
         self.l_player.get_card(Lamb(1))
         self.l_player.get_card(Crab(1))
         self.r_player.get_card(Lamb(-1))
 
-    def check_out_of_range(self,x,y):
-        if (x,y) in self.board_range:
-            return False
-        else:
-            return True
-
-    def display_board(self):
-        for j in self.board:
-            output = ''
-            for i in j:
-                if i is None:
-                    output = output + (' ~~ ')
-                else:
-                    output = output + i.__str__()
-            print(output)
 
     def play_card(self, player, player_opp, orig_loc, card, action):
         '''Example:
@@ -87,14 +105,15 @@ class Game:
 
         orig_loc_x,orig_loc_y = orig_loc
         d_x,d_y = delta
-        unit = self.board[orig_loc_x][orig_loc_y]
+        target_loc = (orig_loc_x + d_x,orig_loc_y + d_y)
+        unit = self.board.get_piece(orig_loc)
         if unit is None:
             return 'no unit at location'
         if unit.owner != player:
             return 'unit does not belong to player'
-        if self.check_out_of_range(orig_loc_x + d_x,orig_loc_y + d_y):
+        if self.board.check_out_of_range(target_loc):
             return 'target location is out of range'
-        target = self.board[orig_loc_x + d_x][orig_loc_y + d_y]
+        target = self.board.get_piece(target_loc)
         if target is not None:
             if target.owner == player:
                 return 'target location occupied by friendly unit'
@@ -110,13 +129,16 @@ class Game:
 
         orig_loc_x,orig_loc_y = orig_loc
         d_x,d_y = delta
-        unit = self.board[orig_loc_x][orig_loc_y]
-        self.board[orig_loc_x][orig_loc_y] = None
-        self.board[orig_loc_x + d_x][orig_loc_y + d_y] = unit
+        target_loc = (orig_loc_x + d_x,orig_loc_y + d_y)
+        unit = self.board.get_piece(orig_loc)
+        self.board.piece_dict.pop(orig_loc)
+        self.board.piece_dict[target_loc] = unit
 
 
-# a = Game()
-# a.play_card(a.l_player,a.r_player,(0,2),a.l_player.card_list[1],2)
-# a.display_board()
+a = Game()
+
+a.play_card(a.l_player,a.r_player,(0,2),a.l_player.card_list[1],2)
+# print(a.board.piece_dict)
+a.board.display()
 # a.r_player.show_cards()
 
